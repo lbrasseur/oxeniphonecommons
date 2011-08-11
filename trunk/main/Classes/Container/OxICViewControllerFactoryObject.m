5a2e3303723a5a94c033f729d31fc88c33847e76
lbrasseur <lbrasseur@37a52bc6-cbfe-079c-badf-032faa60eb6c>
2011-08-11 21:11:44 +0000

    Added factory for view controllers

---
+++ trunk/main/Classes/Container/OxICViewControllerFactoryObject.m
@@ -0,0 +1,28 @@
+//
+//  OxICViewControllerFactoryObject.m
+//  OxeniPhoneCommons
+//
+//  Created by Lautaro Brasseur on 11/08/11.
+//  Copyright 2011 Oxen Software Studio. All rights reserved.
+//
+
+#import "OxICViewControllerFactoryObject.h"
+#import <UIKit/UIKit.h>
+
+@implementation OxICViewControllerFactoryObject
+@synthesize controllerClass, nibName;
+
+- (void) dealloc {
+	self.controllerClass = nil;
+	self.nibName = nil;
+	[super dealloc];
+}
+
+
+- (id) getObject {
+	UIViewController *controller = [objc_getClass([self.controllerClass UTF8String]) new];
+	[controller initWithNibName:self.nibName bundle:nil];
+	return [controller autorelease];
+}
+
+@end
