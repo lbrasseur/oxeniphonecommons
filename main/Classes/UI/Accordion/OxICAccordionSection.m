72ae35cd3778e0fbdedd90935abf397f8b6ab2f1
lbrasseur <lbrasseur@37a52bc6-cbfe-079c-badf-032faa60eb6c>
2011-08-18 13:19:49 +0000

    Accordion

---
+++ main/Classes/UI/Accordion/OxICAccordionSection.m
@@ -0,0 +1,72 @@
+//
+//  OxICAccordionSection.m
+//  RecetarioiPhone
+//
+//  Created by Lautaro Brasseur on 18/08/11.
+//  Copyright 2011 Oxen Software Studio. All rights reserved.
+//
+
+#import "OxICAccordionSection.h"
+
+@interface OxICAccordionSection()
+@property (nonatomic, retain) OxICAccordion *parent;
+@property (nonatomic, assign) BOOL collapsedFlag;
+@property (nonatomic, assign) int position;
+@property (nonatomic, retain) UIView* content;
+@end
+
+
+@implementation OxICAccordionSection
+@synthesize parent, collapsedFlag, position, collapsedHeight, expandedHeight, content;
+
+# pragma mark Init and dealloc
+- (id) initWithFrame:(CGRect)frame
+		   andParent:(OxICAccordion*) parentAccordion
+		    andTitle:(NSString*) title
+		  andContent:(UIView*) sectionContent
+		 andPosition:(int) sectionPosition {
+	
+    if ((self = [super initWithFrame:frame])) {
+		self.parent = parentAccordion;
+		self.collapsedFlag = YES;
+		self.position = sectionPosition;
+		self.content = sectionContent;
+		collapsedHeight = frame.size.height;
+		expandedHeight = frame.size.height + content.frame.size.height;
+		
+		UIButton *labelButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+		[labelButton setTitle:title forState:UIControlStateNormal];
+		labelButton.frame = CGRectMake(0, 0, frame.size.width, frame.size.height);
+		[labelButton addTarget:self action:@selector(buttonClick:) forControlEvents:UIControlEventTouchUpInside];
+		
+		[self addSubview:labelButton];
+		
+		self.content.frame = CGRectMake(0, self.frame.size.height, content.frame.size.width, content.frame.size.height);		
+    }
+    return self;
+}
+
+- (void) dealloc {
+	self.parent = nil;
+	self.content = nil;
+    [super dealloc];
+}
+
+#pragma mark Interface methods
+- (void) collapse {
+	self.collapsedFlag = YES;
+	[self.content removeFromSuperview];
+}
+
+- (void) expand {
+	self.collapsedFlag = NO;
+	[self addSubview:self.content];
+}
+
+
+#pragma mark Events
+- (void) buttonClick:(id) sender {
+	[self.parent setCollapsed:!self.collapsedFlag toSection:self.position];
+}
+
+@end
