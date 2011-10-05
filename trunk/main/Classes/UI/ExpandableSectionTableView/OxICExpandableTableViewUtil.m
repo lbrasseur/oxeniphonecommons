401845a200f7fca6a68d63e727768e7950b89f50
ffumaneri@gmail.com <ffumaneri@gmail.com@37a52bc6-cbfe-079c-badf-032faa60eb6c>
2011-10-05 23:45:42 +0000

    Agrego una utilidad para expandir y contraer filas de una sección en un table view

---
+++ trunk/main/Classes/UI/ExpandableSectionTableView/OxICExpandableTableViewUtil.m
@@ -0,0 +1,124 @@
+//
+//  OxICExpandableTableViewDelegate.m
+//  OxeniPhoneCommons
+//
+//  Created by Facundo Fumaneri on 10/5/11.
+//  Copyright 2011 Oxen. All rights reserved.
+//
+
+#import "OxICExpandableTableViewUtil.h"
+
+@interface OxICTargetDelegate : NSObject {
+	NSInteger section;
+	OxICExpandableTableViewUtil *util;
+	UITableView* tableView;
+	id userInfo;
+}
+
+@property (assign, nonatomic) NSInteger section;
+@property (assign, nonatomic) id userInfo;
+@property (retain, nonatomic) OxICExpandableTableViewUtil *util;
+@property (retain, nonatomic) UITableView* tableView;
+
+-(void) sectionControlClick:(id) sender;
+
+@end
+
+@implementation OxICTargetDelegate
+@synthesize section, util, tableView, userInfo;
+
+-(void) sectionControlClick:(id) sender {
+	[util tableView:tableView switchVisibility:section userInfoOrNil:userInfo];
+}
+
+-(void) dealloc {
+	[super dealloc];
+	[util release];
+	[tableView release];
+	userInfo = nil;
+}
+@end
+
+@interface OxICExpandableTableViewUtil()
+
+@property (nonatomic, retain) NSMutableArray *sectionTargets;
+@property (nonatomic, retain) NSMutableDictionary *sectionVisibility;;
+
+@end
+
+
+@implementation OxICExpandableTableViewUtil
+@synthesize sectionTargets, sectionVisibility, delegate;
+
+-(id) init {
+	if (self = [super init]) {
+		self.sectionTargets = [[NSMutableArray alloc] init]; 
+		self.sectionVisibility = [[NSMutableDictionary alloc] init];
+	}
+	return self;
+}
+-(void) tableView:(UITableView*)tableView switchVisibility:(NSInteger) section userInfoOrNil:(id)userInfo{
+	[self tableView:tableView setVisibilityWithSection:section andVisible:![self isSectionVisible:section] userInfoOrNil:userInfo];
+}
+
+-(void) tableView:(UITableView*)tableView setVisibilityWithSection:(NSInteger) section andVisible:(BOOL) visible userInfoOrNil:(id)userInfo{
+	[self.sectionVisibility setObject:[NSNumber numberWithBool:visible] forKey:[NSNumber numberWithInt:section]];
+	[tableView beginUpdates];
+	if (visible) {
+		NSMutableArray *indexPaths = [[NSMutableArray alloc] init];
+		NSInteger rows = [tableView.dataSource tableView:tableView numberOfRowsInSection:section];
+		for (int i = 0; i < rows; i++) {
+			[indexPaths addObject:[NSIndexPath indexPathForRow:i inSection:section]];
+		}
+		[tableView insertRowsAtIndexPaths:indexPaths withRowAnimation:UITableViewRowAnimationTop];
+		[indexPaths release];
+	} else {
+		NSMutableArray *indexPaths = [[NSMutableArray alloc] init];
+		NSInteger rows = [tableView numberOfRowsInSection:section];
+		for (int i = 0; i < rows; i++) {
+			[indexPaths addObject:[NSIndexPath indexPathForRow:i inSection:section]];
+		}
+
+		[tableView deleteRowsAtIndexPaths:indexPaths withRowAnimation:UITableViewRowAnimationTop];
+		[indexPaths release];
+	}
+	[tableView endUpdates];
+	if (visible) {
+		if (delegate) {
+			[delegate tableView:tableView didExpandSection:section userInfo:userInfo];
+		}
+	} else {
+		if (delegate) {
+			[delegate tableView:tableView didCollapseSection:section userInfo:userInfo];
+		}
+	}
+}
+
+-(void) tableView:(UITableView*)tableView addControl:(id) control forSection:(NSInteger) section userInfoOrNil:(id)userInfo {
+	OxICTargetDelegate *target = [[OxICTargetDelegate alloc] init];
+	[target setSection:section];
+	[target setUtil:self];
+	[target setTableView:tableView];
+	[target setUserInfo:userInfo];
+	[control addTarget:target action:@selector(sectionControlClick:) forControlEvents:UIControlEventTouchUpInside];
+	[self.sectionTargets addObject:target];
+	[target release];
+}
+
+-(BOOL) isSectionVisible:(NSInteger) section {
+	NSNumber *visible = [self.sectionVisibility objectForKey:[NSNumber numberWithInt:section]];
+	if (!visible) {
+		[self.sectionVisibility setObject:[NSNumber numberWithBool:NO] forKey:[NSNumber numberWithInt:section]];
+		return NO;
+	}
+	return [visible boolValue];
+}
+
+
+-(void) dealloc {
+	[super dealloc];
+	[sectionTargets release];
+	[sectionVisibility release];
+}
+
+@end
