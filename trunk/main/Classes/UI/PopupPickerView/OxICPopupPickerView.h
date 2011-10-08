944a307a66a2c38cc55fe3392f0f8b254b9d6b03
lbrasseur@gmail.com <lbrasseur@gmail.com@37a52bc6-cbfe-079c-badf-032faa60eb6c>
2011-10-08 23:07:58 +0000

    popup picker view and adjustments for interface builder compatibility

---
+++ trunk/main/Classes/UI/PopupPickerView/OxICPopupPickerView.h
@@ -0,0 +1,26 @@
+//
+//  OxICPopupPickerView.h
+//  OxeniPhoneCommons
+//
+//  Created by Lautaro Brasseur on 08/10/11.
+//  Copyright 2011 Oxen Software Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface OxICPopupPickerView : UIView<UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource, UIActionSheetDelegate> {
+	UITextField *textField;
+	NSMutableArray *identifiers;
+	NSMutableArray *labels;
+	NSInteger selectedRow;
+	NSInteger pickerSelectedRow;
+}
+
+- (void) addOption:(id) identifier
+		 withLabel:(NSString*) label;
+
+@property (nonatomic, retain) id selected;
+
+
+@end
---
+++ trunk/main/Classes/UI/PopupPickerView/OxICPopupPickerView.m
@@ -0,0 +1,131 @@
+//
+//  OxICPopupPickerView.m
+//  OxeniPhoneCommons
+//
+//  Created by Lautaro Brasseur on 08/10/11.
+//  Copyright 2011 Oxen Software Studio. All rights reserved.
+//
+
+#import "OxICPopupPickerView.h"
+
+@interface OxICPopupPickerView()
+- (void) showPicker;
+@property (nonatomic, retain) NSMutableArray *identifiers;
+@property (nonatomic, retain) NSMutableArray *labels;
+@end
+
+@implementation OxICPopupPickerView
+@synthesize labels, identifiers;
+
+#pragma mark Initialization and dealloc
+- (void)initialize {
+	self.identifiers = [NSMutableArray arrayWithCapacity:10];
+	self.labels = [NSMutableArray arrayWithCapacity:10];
+	
+	textField = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
+	textField.borderStyle = UITextBorderStyleRoundedRect;
+	textField.delegate = self;
+	[self addSubview:textField];
+	[textField release];
+}
+
+- (void)awakeFromNib {
+	[self initialize];
+}
+
+- (id)initWithFrame:(CGRect)frame {
+    if ((self = [super initWithFrame:frame])) {
+		[self initialize];
+    }
+    return self;
+}
+
+- (void)dealloc {
+	self.identifiers = nil;
+	self.labels = nil;
+    [super dealloc];
+}
+
+#pragma mark Interface methods
+- (void) addOption:(id) identifier
+		 withLabel:(NSString*) label {
+	[self.identifiers addObject:identifier];
+	[self.labels addObject:label];
+}
+
+- (id) selected {
+	if (selectedRow >= 0 && selectedRow < [self.identifiers count]) {
+		return [self.identifiers objectAtIndex:selectedRow];
+	} else {
+		return nil;
+	}
+}
+
+- (void) setSelected:(id) identifier {
+	selectedRow = -1;
+	textField.text = nil;
+	for (NSInteger n = 0; n < [self.identifiers count]; n++) {
+		id current = [self.identifiers objectAtIndex:n];
+		if ([current isEqual:identifier]) {
+			selectedRow = n;
+			textField.text = [self.labels objectAtIndex:selectedRow];
+			break;
+		}
+	}
+}
+
+#pragma mark Private methods
+- (void) showPicker {
+	UIActionSheet *menu = [[UIActionSheet alloc] initWithTitle:nil
+													  delegate:self
+											 cancelButtonTitle:NSLocalizedString(@"Done", @"Done")
+										destructiveButtonTitle:NSLocalizedString(@"Cancel", @"Cancel")
+											 otherButtonTitles:nil];
+	
+	// Add the picker
+	UIPickerView *pickerView = [[UIPickerView alloc] initWithFrame:CGRectMake(0,150,0,216)];
+	
+	pickerView.delegate = self;
+	pickerView.showsSelectionIndicator = YES;
+	
+	[pickerView selectRow:selectedRow inComponent:0 animated:NO];
+	
+	[menu addSubview:pickerView];
+	[menu showInView:[[UIApplication sharedApplication] keyWindow]];
+	[menu setBounds:CGRectMake(0,0,320, 575)];
+	
+	[pickerView release];
+	[menu release];	
+}
+
+
+#pragma mark UITextFieldDelegate methods
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+	[self showPicker];
+	return NO;
+}
+
+#pragma mark UIPickerViewDataSource and UIPickerViewDelegate methods
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+	return (NSString*)[self.labels objectAtIndex:row];
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+	return 1;
+}
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+	return [self.labels count];
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
+	pickerSelectedRow = row;
+}
+
+#pragma mark UIActionSheetDelegate methods
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex{
+	if (buttonIndex == 1) {
+		selectedRow = pickerSelectedRow;
+		textField.text = [self.labels objectAtIndex:selectedRow];
+	}
+}
+@end
